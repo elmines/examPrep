a46aa7fa3c731c1626b300a02c91cb586daeadb3
Ethan Mines <elmines@users.noreply.github.com>
2017-04-30 17:36:36 -0500

    Verify basic functionality of Graph::DFS() using Dr. Dixon's graph from the second exam

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "Graph.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		cout << "No file given." << endl;
+		return 1;
+	}
+
+	Graph g(argv[1]);
+	g.PrintAdjacencyList();
+	g.DFS(0);
+	return 0;
+}
